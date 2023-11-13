512628138588e56c1522b564ca67b6fe0c45ce22
Cenafowzin <digodubeux@gmail.com>
2023-11-13 17:57:45 -0300

    force 32

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-gcc -I src/ -L src/lib src/Game/*.c -o game game.c -Wall -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf
+gcc -m32 -I src/ -L src/lib src/Game/*.c -o game game.c -Wall -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf
