c796a9698eabfe11c91bfd4ec35ae061577282bb
Flávio Muniz <116359369+flavio-muniz@users.noreply.github.com>
2023-11-13 09:22:49 -0300

    Update build.bat
    forçando 32bits

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-gcc -I src/include/SDL2 -L src/lib -o game game.c -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf
+gcc -m32 -I src/include/SDL2 -L src/lib -o game game.c -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf
